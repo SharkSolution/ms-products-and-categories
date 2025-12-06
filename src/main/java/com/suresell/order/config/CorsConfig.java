964f92a112c96403a2ff3068c796e005880b90ce
GuzmanAndrew <ramirezandrew172@gmail.com>
2025-12-05 21:16:10 -0500

    change orders

---
+++ src/main/java/com/suresell/order/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.suresell.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Solo permite red Tailscale http://100.*.*.*:4200
+        config.addAllowedOriginPattern("*");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
